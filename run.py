669f94ddad0c6831216a4bcb0a719b560e90cc25
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-10-03 14:47:43 +0200

    creada una función para elegir la interfaz o terminal, y colorama para mejorar el diseño en terminal

--- run.py
+++ run.py
@@ -1,4 +1,13 @@
 import menu
+from colorama import Fore
+from ui import MainWindow
 
-if __name__ == "__main__":
-   menu.iniciar()
+def seleccionar():
+   eleccion = int(input(Fore.LIGHTGREEN_EX + "Pulse 1 para ejecutarlo en terminal o 2 para ejecutarlo en interfaz gráfica\n >"))
+   if eleccion  == 1:
+      menu.iniciar()
+
+   if eleccion == 2:
+      app = MainWindow()
+      app.mainloop()
+seleccionar()
