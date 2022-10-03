79571eefbafc136f4f81a30d8c4bca90ed61ddde
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-10-03 14:49:04 +0200

    añadido u espacio para hacerlo más legible

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from colorama import Fore
 from ui import MainWindow
 
 def seleccionar():
-   eleccion = int(input(Fore.LIGHTGREEN_EX + "Pulse 1 para ejecutarlo en terminal o 2 para ejecutarlo en interfaz gráfica\n >"))
+   eleccion = int(input(Fore.LIGHTGREEN_EX + "Pulse 1 para ejecutarlo en terminal o 2 para ejecutarlo en interfaz gráfica\n > "))
    if eleccion  == 1:
       menu.iniciar()
 
