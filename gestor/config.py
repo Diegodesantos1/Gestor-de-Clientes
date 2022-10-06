f604e8b46e10662981f05bfbef0c7c88748d703e
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-10-06 19:21:23 +0200

    arreglado y ya funciona con el run fuera de gestor

--- gestor/config.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-DATABASE_PATH = "./gestor/clientes.csv"
-
-if "pytest" in sys.argv[0]:
-    DATABASE_PATH = "./gestor/test/clientes_test.csv"
--- run.py
+++ run.py
@@ -1,7 +1,10 @@
-import menu as menu
+import sys
+import pathlib
+sys.path.append(str(pathlib.Path().resolve()) + './gestor')
+sys.path.append
+import menu
 from colorama import Fore
 from ui import MainWindow
-import test.test_database as TD
 
 def seleccionar():
    eleccion = int(input(Fore.LIGHTGREEN_EX + "\n Bienvenido al menú de selección: \n\n Pulse 1 para ejecutarlo en terminal \n Pulse 2 para ejecutarlo en interfaz gráfica\n > " + Fore.RESET))
